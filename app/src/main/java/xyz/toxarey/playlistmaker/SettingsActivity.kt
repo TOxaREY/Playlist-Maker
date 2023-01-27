b28bd3ceaa3f5ce0470d32915662ed178c6787ca
TOxaREY <delta@toxarey.xyz>
2023-01-27 23:07:32 +0300

    sprint_08 v1.0

---
+++ app/src/main/java/xyz/toxarey/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package xyz.toxarey.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backToMainButton = findViewById<Button>(R.id.back_to_main_button)
+
+        backToMainButton.setOnClickListener {
+            val backToMainIntent = Intent(this, MainActivity::class.java)
+            startActivity(backToMainIntent)
+        }
+    }
+}
