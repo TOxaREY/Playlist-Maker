fcefa851dd258e9b992e33ab02144bbf47eb68bb
Anton Reynikov <toxareyapp@yandex.ru>
2023-02-17 11:35:07 +0300

    sprint_09 v1.0

--- app/src/main/java/xyz/toxarey/playlistmaker/SearchActivity.kt
+++ app/src/main/java/xyz/toxarey/playlistmaker/SearchActivity.kt
@@ -1,11 +1,74 @@
 package xyz.toxarey.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+    lateinit var searchEditText: EditText
+    var searchText = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backToMainButton = findViewById<Button>(R.id.back_to_main_button_from_search)
+        searchEditText = findViewById(R.id.search_edit_text)
+        val clearSearchButton = findViewById<Button>(R.id.clear_search_button)
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+
+        backToMainButton.setOnClickListener {
+            val backToMainIntent = Intent(this, MainActivity::class.java)
+            startActivity(backToMainIntent)
+            finish()
+        }
+        clearSearchButton.setOnClickListener {
+            searchEditText.setText("")
+            searchText = ""
+            inputMethodManager?.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearSearchButton.visibility = clearSearchButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                searchText = searchEditText.text.toString()
+            }
+        }
+        searchEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchEditText.setText(savedInstanceState.getString(SEARCH_TEXT, ""))
+        searchEditText.setSelection(searchEditText.text.length)
+    }
+
+    private fun clearSearchButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/xyz/toxarey/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/xyz/toxarey/playlistmaker/SettingsActivity.kt
@@ -2,22 +2,62 @@ package xyz.toxarey.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    lateinit var darkThemeSwitch: androidx.appcompat.widget.SwitchCompat
+    @SuppressLint("MissingInflatedId", "UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backToMainButton = findViewById<Button>(R.id.back_to_main_button)
+        val backToMainButton = findViewById<Button>(R.id.back_to_main_button_from_settings)
+        darkThemeSwitch = findViewById(R.id.dark_theme_switch)
+        val shareAppButton = findViewById<ImageView>(R.id.share_app_image_view)
+        val supportButton = findViewById<ImageView>(R.id.support_image_view)
+        val termsOfUseButton = findViewById<ImageView>(R.id.terms_of_use_image_view)
 
         backToMainButton.setOnClickListener {
             val backToMainIntent = Intent(this, MainActivity::class.java)
             startActivity(backToMainIntent)
             finish()
         }
+        checkDarkTheme()
+        darkThemeSwitch.setOnCheckedChangeListener { _, b ->
+            if (b) {
+               AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        shareAppButton.setOnClickListener {
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_address))
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_app)))
+        }
+        supportButton.setOnClickListener {
+            val supportIntent = Intent()
+            supportIntent.action = Intent.ACTION_SENDTO
+            supportIntent.data = Uri.parse("mailto:" + getString(R.string.my_email))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.theme_email))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.text_email))
+            startActivity(Intent.createChooser(supportIntent, getString(R.string.write_to_support)))
+        }
+        termsOfUseButton.setOnClickListener {
+            val termsOfUseIntent = Intent()
+            termsOfUseIntent.action = Intent.ACTION_VIEW
+            termsOfUseIntent.data = Uri.parse(getString(R.string.terms_of_use_text))
+            startActivity(Intent.createChooser(termsOfUseIntent, getString(R.string.terms_of_use)))
+        }
+    }
+    private fun checkDarkTheme() {
+        darkThemeSwitch.isChecked = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
     }
 }
